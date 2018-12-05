6b8fafac3c3839adcaaf3b45c0b7987c019eaac2
Peyton Tanzillo <ptown16@gmail.com>
2018-12-05 11:42:03 -0600

    2 piece jumps successfully work.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,23 @@ class Board extends Component {
     super(props);
     this.state = {
       currentColorIndex: 0,
+      hasGridGenerated: false,
+      pieceGrid: Array(360).fill(false)
     };
   }
   
+  xyPosToArray(xPos, yPos) {
+    return xPos + (19 * yPos);
+  }
+  
   renderBox(xPos, yPos) {
-    return <Box x={xPos} y={yPos} onClick={() => this.renderPiece()}/>;
+    let hasPiece = undefined;
+    if (this.state.pieceGrid[this.xyPosToArray(xPos, yPos)] === false) {
+      hasPiece = false;
+    } else {
+      hasPiece = true;
+    } 
+    return <Box x={xPos} y={yPos} hasPiece={hasPiece} onClick={() => this.renderPiece(xPos, yPos)}/>;
   }
     
   renderRow(rowPos) {
@@ -19,8 +31,8 @@ class Board extends Component {
     for (let i = 0; i < this.props.boardSize; i++) {
       row.push(this.renderBox(i, rowPos));
     }
-    const keyedRow = row.map((box) =>
-        <div className="square" key={box.props.x + box.props.y * this.props.boardSize}>{box}</div>
+    const keyedRow = row.map((box, i) =>
+        <div className="square" key={i}>{box}</div>
     );
     return keyedRow;
   }
@@ -30,21 +42,49 @@ class Board extends Component {
     for (let i = 0; i < this.props.boardSize; i++) {
       column.push(this.renderRow(i));
     }
-    const keyedColumn = column.map((row) =>
-      <div className="row" key={row[0].key}>{row}</div>
+    const keyedColumn = column.map((row, i) =>
+      <div className="row" key={i}>{row}</div>
     );
-    console.log(keyedColumn);
     return keyedColumn;
   }
   
-  renderPiece() {
+  testSingleJump(xPos, yPos, xInterval, yInterval) {
+    const jump1 = this.state.pieceGrid[this.xyPosToArray(xPos + xInterval, yPos + yInterval)]; 
+    if (jump1 !== false) {
+      const start = this.state.pieceGrid[this.xyPosToArray(xPos, yPos)];
+      if (jump1 !== start) {
+        const jump2 = this.state.pieceGrid[this.xyPosToArray(xPos + (2 * xInterval), yPos + (2 * yInterval))];
+        if (jump1 === jump2) {
+          const jump3 = this.state.pieceGrid[this.xyPosToArray(xPos + (3 * xInterval), yPos + (3 * yInterval))];
+          if (jump3 === start) {
+            this.state.pieceGrid[this.xyPosToArray(xPos + xInterval, yPos + yInterval)] = false;
+            this.state.pieceGrid[this.xyPosToArray(xPos + (2 * xInterval), yPos + (2 * yInterval))] = false;
+          }
+        }
+      }
+    }
+  }
+  
+  renderPiece(xPos, yPos) {
     let i = this.state.currentColorIndex;
+    this.state.pieceGrid[xPos + (19 * yPos)] = i;
     if (this.state.currentColorIndex >= this.props.colors.length - 1) {
       this.setState({currentColorIndex: 0});
     } else {
       this.setState({currentColorIndex: i + 1});
     }
+    this.updateJumps(xPos, yPos);
     return (<div className="dot" style={{backgroundColor: this.props.colors[i]}}></div>);
+  }
+            
+  updateJumps(xPos, yPos) {
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (i !== 0 || j !== 0) {
+          this.testSingleJump(xPos, yPos, i, j);
+        }
+      }
+    }
   }
     
   render() {
@@ -60,13 +100,12 @@ class Box extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hasPiece: false,
       piece: undefined,
     };
   } 
   
   render() {
-    if (this.state.hasPiece) {
+    if (this.props.hasPiece) {
       return (
         <div className="square">
           {this.state.piece}
@@ -82,7 +121,6 @@ class Box extends Component {
   
   clickHandler() {
     this.setState({
-      hasPiece: true,
       piece: this.props.onClick(),
     });
   }
@@ -91,9 +129,12 @@ class Box extends Component {
 class Game extends Component {
   
   render() {
+    const board = <Board boardSize={19} colors={["#123456", "blue"]}/>;
     return (
-      <div className="App">
-        <Board boardSize={19} colors={["#0A2239", "#A50005", "#087E8B", "#345830", "#FF4D80"]}/>
+      <div>
+        <div className="App">
+          {board}
+        </div>
       </div>
     );
   }
