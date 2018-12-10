0b4e326af07e85eb88e11a5a5489523f4fd75390
Peyton Tanzillo <ptown16@gmail.com>
2018-12-10 12:34:10 -0600

    Added Scoreboard.js to display game state (not functional)

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Board from './Board.js'
+import Scoreboard from './Scoreboard.js'
 
 
 // Container Component for Board with, in future, extra information.
 class Game extends Component {
-  
+
   render() {
+    const player1Color = "rgba(0, 180, 180, 1)";
+    const player2Color = "rgba(200, 0, 180, 1)";
     return (
       <div>
         <h2 className="title">Pente Web</h2>
         <div className="App">
-          <Board boardSize={19} colors={["#123456", "blue"]} />
+          <Scoreboard playerID={0} playerColor={player1Color} pairsJumped={2} />
+          <Board boardSize={19} colors={[player1Color, player2Color]} />
+          <Scoreboard playerID={1} playerColor={player2Color} pairsJumped={4} />
         </div>
       </div>
     );
---
+++ src/Scoreboard.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Scoreboard extends Component {
+  constructor(props) {
+    super(props);
+    this.unfilledColor = this.getUnfilledColor();
+  }
+
+  getUnfilledColor() {
+      const filledColor = this.props.playerColor.split(',');
+      return `${filledColor[0]},${filledColor[1]},${filledColor[2]}, 0.3)`;
+  }
+
+  renderCircles() {
+    const circles = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < this.props.pairsJumped) {
+        circles.push(<div className="pair" key={i} style={{backgroundColor: this.props.playerColor}}></div>);
+      } else {
+        circles.push(<div className="pair unfilled" key={i} style={{backgroundColor: this.unfilledColor}}></div>);
+      }
+    }
+    return circles;
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <h2 style={{color: this.props.playerColor}}>Player {this.props.playerID + 1}</h2>
+        <div className="circles">
+          <p>Pairs Jumped:</p>
+          {this.renderCircles()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Scoreboard;
